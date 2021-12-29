0e6936846618b00686e0019e315eaced92bc8470
ekramasif <asib43@gmail.com>
2021-12-29 16:40:16 +0600

    Divisible by a Given Number added

--- basics.go
+++ basics.go
@@ -11,4 +11,13 @@ func main() {
 	for i := 2; i <= 10; i = i + 2 {
 		fmt.Println(i)
 	}
+
+	var n int
+	fmt.Printf("Enter a number For Divisible n: ")
+	fmt.Scanf("%d", &n)
+	for i := 1; i <= 20; i++ {
+		if i%n == 0 {
+			fmt.Println(i)
+		}
+	}
 }
